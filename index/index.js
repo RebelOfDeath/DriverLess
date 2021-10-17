26671c9aea45e86e290eaab0f06a1e0eed803237
Roham <koohestaniroham@gmail.com>
2021-10-17 12:47:12 +0200

    added typewriter

--- index/index.js
+++ index/index.js
@@ -2,8 +2,63 @@ setTimeout(function(){
  document.getElementById('arrow-down').classList.remove('hide');
 }, 7000);
 
-var img = document.getElementById('test-image');
-var width = img.clientWidth;
-var vw = window.innerWidth;
-var leftimg = (vw / 2) - (width / 2);
-document.getElementById('test-iamge').style.left = leftimg + "px";
+// var img = document.getElementById('test-image');
+// var width = img.clientWidth;
+// var vw = window.innerWidth;
+// var leftimg = (vw / 2) - (width / 2);
+// document.getElementById('test-image').style.left = leftimg + "px";
+
+//region for typewriter animation of intro modules section
+//#region
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+             }
+        }
+    };
+//#endregion
