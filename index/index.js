7d2168c9564acede21594778b02f4d51f71f4ed7
Roham <koohestaniroham@gmail.com>
2021-10-17 10:39:39 +0200

    small change to div
    changed the layout of the page by changing some properties in the css

--- index/index.js
+++ index/index.js
@@ -1,3 +1,9 @@
 setTimeout(function(){
  document.getElementById('arrow-down').classList.remove('hide');
 }, 7000);
+
+var img = document.getElementById('test-image');
+var width = img.clientWidth;
+var vw = window.innerWidth;
+var leftimg = (vw / 2) - (width / 2);
+document.getElementById('test-iamge').style.left = leftimg + "px";
