100ab99b348df37b612d790993dd1f3d172bede6
Roham <koohestaniroham@gmail.com>
2021-10-30 17:38:24 +0200

    added lay-out for Arduino page & fixed menu bug
    fixed the spacing bug of ham menu and added lay-out for the Arduino page

--- Arduino/arduino.js
+++ Arduino/arduino.js
@@ -1,3 +1,20 @@
 setTimeout(function(){
     document.getElementById('arrow-down').classList.remove('hide');
    }, 7000);
+
+const typewriter = document.querySelector(".typewriter")
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("typewriter-animation");
+      document.getElementById('frame_for_recom').classList.add('fade_in_form');
+    }
+  })
+}
+
+const options = {}
+
+const myObserver = new IntersectionObserver(callback, options)
+
+myObserver.observe(typewriter)
