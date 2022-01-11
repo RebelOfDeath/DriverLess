5b47809544b4cf4ab193d353db653bdd1baa4cd5
revib0911 <90382213+revib0911@users.noreply.github.com>
2022-01-11 10:49:59 +0100

    Corrected typing mistakes componenten

--- Componenten/componenten.js
+++ Componenten/componenten.js
@@ -99,61 +99,61 @@
 var post = [
   {
     postTitle: "LED (Light Emmiting Diode)",
-    postContent: "<h3>Wat doet het?</h3><p>Als er een klein beetje stroom doorheen loopt geeft de led licht. De stroom kan er maar in een richting door, net als bij een diode. Vandaar de naam led : light emitting diode.</p><h3>Waar let je op?</h3><p>Altijd aansluiten met een weerstand (bv 330 ohm), anders gaat hij stuk, en het lange pootje moet aan d plus kan, het korte aan de min.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Als er een klein beetje stroom doorheen loopt geeft de led licht. De stroom kan er maar in een richting door, net als bij een diode. Vandaar de naam LED: Light Emitting Diode.</p><h3>Waar moet je op letten?</h3><p>Altijd aansluiten met een weerstand (bv 330 ohm), anders gaat hij stuk, en het lange pootje moet aan de plus kant, het korte aan de min.</p>",
     postImg: "https://upload.wikimedia.org/wikipedia/commons/c/c8/5mm_Red_LED.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Diode",
-    postContent: "<h3>Wat doet het?</h3><p>Een diode laat de stroom maar in een richting door. Het is meestal een cilindertje waaruit twee draden komen. aan een kant is een ring waaraaan je kan zien welke kant de plus en min komen.</p><h3>Waar let je op?</h3><p>Een diode kan je snel verkeerd aansluiten en dan werkt je circuit niet of er kan iets kapot gaaan. De ring moet aan de min kant worden aangesloten.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een diode laat de stroom maar in 1 richting door. Het is meestal een cilindertje waaruit twee draden komen. Aan een kant is een ring waaraan je kan zien welke kant de plus en de min zijn.</p><h3>Waar moet je op letten?</h3><p>Een diode kan je snel verkeerd aansluiten en dan werkt je circuit niet of er kan iets kapot gaan. De ring moet aan de minkant worden aangesloten.</p>",
     postImg: "https://kapottespullen.nl/wp-content/uploads/2020/11/Diode.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Weerstand",
-    postContent: "<h3>Wat doet het?</h3><p>Een weerstand vermidert de stroom in een circuit zodat alle onderdelen de juiste hoeveelheid stroom krijgen. DE waarde van de weerstand wordt uigedrukt in ohm.</p><h3>Waar let je op?</h3><p>Ze zijn klein en moeilijk van elkaar te onderscheiden. Aan gekleurde ringen kan je de waarde aflezen. je kan het ook meten met een multimeter.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een weerstand vermindert de stroom in een circuit zodat alle onderdelen de juiste hoeveelheid stroom krijgen. De waarde van de weerstand wordt uitgedrukt in ohm.</p><h3>Waar moet je op letten?</h3><p>Ze zijn klein en moeilijk van elkaar te onderscheiden. Aan gekleurde ringen kan je de waarde aflezen. Je kan het ook meten met een multimeter.</p>",
     postImg: "https://cdn2.soldeerboutje.nl/3672/velleman-ra3e9-weerstand-39ohm-100stuks.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Transistor",
-    postContent: "<h3>Wat doet het?</h3><p>Een transistor gebruikt een klein stoompje om een veel grotere stroom te schakelen of te versterken. Er gaat een stroom (of meer) stroom lopen  als er een kleine stroom door de basis gaat.</p><h3>Waar let je op?</h3><p>Zorg dat het collector pootje aan de pluskant is en het emitter pootje aan de min kant. Transistors zijn er in veel maten en soorten. Wij gebruiken meestal de 547, 557 of de 2222AG.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een transistor gebruikt een klein stroompje om een veel grotere stroom te schakelen of te versterken. Er gaat een stroompje (of meerdere stroompjes) lopen als er een kleine stroom door de basis gaat.</p><h3>Waar moet je op letten?</h3><p>Zorg dat het collectorpootje aan de pluskant is en het emitterpootje aan de minkant. Transistors zijn er in veel maten en soorten. Wij gebruiken meestal de 547, 557 of de 2222AG.</p>",
     postImg: "https://images-eu.ssl-images-amazon.com/images/I/31Kj%2B54y0tL._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Piezo buzzer",
-    postContent: "<h3>Wat doet het?</h3><p>In een piezo element worden kristallen vervormt door elektriciteit. Een stroom van elektrische pulsen geeft een toon. De behuizing kan ook alleen een metalen schijfje zijn.</p><h3>Waar let je op?</h3><p>De plus en min moeten goed worden aangesloten. Een piezo element kan ook als geluid sensor dienen. De vervorming van de kristallen wekken dan elektriciteit op.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>In een piezo element worden kristallen vervormd door elektriciteit. Een stroom van elektrische pulsen geeft een toon. De behuizing kan ook alleen een metalen schijfje zijn.</p><h3>Waar moet je op letten?</h3><p>De plus en min moeten goed worden aangesloten. Een piezo element kan ook als geluidssensor dienen. De vervorming van de kristallen wekken dan elektriciteit op.</p>",
     postImg: "https://www.distrelec.nl/Web/WebShopImages/landscape_large/_t/if/kingstate-kpeg238.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "IC",
-    postContent: "<h3>Wat doet het?</h3><p>Er zijn heel veel verschillende soorten en maten. In de behuizing zitten complete elektronische circuits. In een ic kunnen enkele tot miljoenen onderdelen zitten.</p><h3>Waar let je op?</h3><p>E ic's die wij gebruiken hebben aan een kant een markering om de pinnen te herkennen, daarnaast blijven ze niet altijd goed in een breadboard.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Er zijn heel veel verschillende soorten en maten. In de behuizing zitten complete elektronische circuits. In een IC kunnen enkele miljoenen onderdelen zitten.</p><h3>Waar moet je op letten?</h3><p>De IC's die wij gebruiken hebben aan een kant een markering om de pinnen te herkennen. Ze blijven niet altijd even goed in het breadboard zitten.</p>",
     postImg: "https://www.haje.nl/images/electronica/halfgeleiders/dil14_640.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Drukknop",
-    postContent: "<h3>Wat doet het?</h3><p>De drukknop diw wij gebruiken maat contact als er op gedruikt wordt. Er zijn ook die net andersom werken. De pootjes passen min of meer in een breadboard.</p><h3>Waar let je op?</h3><p>Kijk goed welke contacten worden doorverbonden bij indrukken. Meet het met de multimeter of controleer met batterij en lampje. als hij niet werkt moet je hem misschien 90 graden draaien.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>De drukknop die wij gebruiken maakt contact als hij ingedrukt wordt. Er zijn ook die net andersom werken. De pootjes passen min of meer in het breadboard.</p><h3>Waar moet je op letten?</h3><p>Kijk goed welke contacten worden doorverbonden bij indrukken. Meet het met de multimeter of controleer het batterij en lampje. Als hij niet werkt moet je hem misschien 90 graden draaien.</p>",
     postImg: "https://arduinofiets.classy.be/drukknop.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "LDR",
-    postContent: "<h3>Wat doet het?</h3><p>Een LDR (light dependent resistor) is een weerstand waarvan de waarde afhangt van de hoeveelheid ilcht die erop valt. In het donker is de weerstand heel hoog, valr er veel licht op dan is de weerstand heel laag.</p><h3>Waar let je op?</h3><p>Het maakt niet uit aan welke kant je de plus of min zet. omdat we met een arduino alleen spanning kunnen registeren en geen weerstand gebruiken we een spanningdeler.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een LDR (Light Dependent Resistor) is een weerstand waarvan de waarde afhangt van de hoeveelheid licht die erop valt. In het donker is de weerstand heel hoog, valt er veel licht op dan is de weerstand heel laag.</p><h3>Waar moet je op letten?</h3><p>Het maakt niet uit aan welke kant je de plus of min zet, omdat we met een arduino alleen spanning kunnen registreren en geen weerstand gebruiken we een spanningsdeler.</p>",
     postImg: "https://cdn.myonlinestore.eu/e40d5160-bac0-4897-baae-d6065a5d5915/image/cache/full/0ba423784fcc5920d0f7fb9e540559b5eee74cfa.jpg",
     postLink: "article-link1"
   },
   {
     postTitle: "Potentiometer",
-    postContent: "<h3>Wat doet het?</h3><p>Een potentiometer is een variabele weerstand. Door aan de middelste pen te draaien verandert de weerstand.</p><h3>Waar let je op?</h3><p>De potentiometer heeft drie contacten. tussen de twee buitenste is het weerstandsmateriaal. Het midelste contact in verbonden met de draaibare loper. deze wordt verbonden met een pin van arduino.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een potentiometer is een variabele weerstand. Door aan de middelste pen te draaien verandert de weerstand.</p><h3>Waar moet je op letten?</h3><p>De potentiometer heeft drie contacten. Tussen de twee buitenste zit het weerstandsmateriaal. Het midelste contact in verbonden met de draaibare loper. Deze wordt verbonden met een pin van de arduino.</p>",
     postImg: "https://www.digikey.nl/-/media/Images/Article%20Library/TechZone%20Articles/2021/May/The%20Fundamentals%20of%20Digital%20Potentiometers%20and%20How%20to%20Use%20Them/article-2021may-the-fundamentals-of-digital-fig1.jpg?la=en&ts=7103c327-388b-4a84-8620-851590a9ec9f",
     postLink: "article-link1"
   },
   {
     postTitle: "Relais",
-    postContent: "<h3>Wat doet het?</h3><p>Een relais is een mechanische schakelaar die bediend wordt door elektrische stoom. Met relais kunnen we groot stromen schakelen zodat we met de arduino ook grotere motoren, lampen etc. kunnen aansturen.</p><h3>Waar let je op?</h3><p>Kijk goed welke contacten schakelen en welke van de elektromagneet zijn.</p>",
+    postContent: "<h3>Wat doet het?</h3><p>Een relais is een mechanische schakelaar die bediend wordt door elektrische stoom. Met relais kunnen we groot stromen schakelen zodat we met de arduino ook grotere motoren, lampen etc. kunnen aansturen.</p><h3>Waarmoet je op letten?</h3><p>Kijk goed welke contacten schakelen en welke van de elektromagneet zijn.</p>",
     postImg: "https://media.s-bol.com/gpD09MmmEjYY/550x449.jpg",
     postLink: "article-link1"
   },
